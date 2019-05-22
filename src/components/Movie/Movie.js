b13cfef2cfb8747e026c423394edf51528761158
Muhammed Nuhman <nuhman7@outlook.com>
2019-05-22 09:05:39 +0530

    fetching api data from movie page

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -10,13 +10,60 @@ import Spinner from '../Spinner/Spinner';
 
 class Movie extends Component {
 
+    state = {
+        movie: null,
+        actors: null,
+        directors: [],
+        loading: false
+    }
+
+    componentDidMount(){
+        this.setState({
+            loading: true
+        });
+        // first fetch the movie
+        const endPoint = `${API_URL}movie/${this.props.match.params.itemId}?api_key=${API_KEY}&language=en-US`;
+        this.fetchItems(endPoint);
+    }
+
+    fetchItems = (endPoint) => {
+        fetch(endPoint)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                if(res.status_code){
+                    this.setState({
+                        loading: false
+                    });
+                } else {
+                    this.setState({
+                        movie: res
+                    }, () => {
+                        const url = `${API_URL}movie/${this.props.match.params.itemId}/credits?api_key=${API_KEY}`;
+                        fetch(url)
+                            .then(res => res.json())
+                            .then(res => {
+                                console.log(res);
+                                const directors = res.crew.filter(member => member.job === "Director");
+                                this.setState({
+                                    actors: res.cast,
+                                    directors,
+                                    loading: false
+                                })
+                            });
+                    });
+                }
+            })
+            .catch(error => console.log('Error while fetching movie details', error));
+    }
+
     render(){
         return(
             <div>
                 <Navigation />
                 <MovieInfo />
                 <MovieInfoBar />
-                
+                <Actor />
                 <Spinner />
             </div>
         );
