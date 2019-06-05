2b1fd9fcc7e6ff5e7a40ebd6342ecbb5a43395df
Muhammed Nuhman <nuhman7@outlook.com>
2019-06-05 13:02:56 +0530

    add top_rated category

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,11 +15,19 @@ class Home extends Component {
     // set the state
     state = {
         movies: [],
+        nowPlayingMovies: [],
+        topRatedMovies: [],
         searchTerm: '',
         welcomeImage: null,
-        loading: false,
-        currentPage: 0,
-        totalPages: 0
+        moviesLoading: false,
+        nowPlayingLoading: false,
+        topRatedLoading: false,
+        moviesCurrentPage: 0,
+        moviesTotalPages: 0,
+        topRatedTotalPages: 0,
+        topRatedCurrentPage: 0,
+        nowPlayingCurrentPage: 0,
+        nowPlayingTotalPages: 0
     }
 
     componentDidMount(){
@@ -27,68 +35,132 @@ class Home extends Component {
         // this run after the page is loaded
         
         this.setState({
-            loading: true // used to show the loading spinner gif
+            moviesLoading: true, // used to show the loading spinner gif,
+            nowPlayingLoading: true
         });
 
         // popular movies url
-        const endPoint = this.getEndPoint("POPULAR");
-        this.fetchItems(endPoint);
+        let endPoint = this.getEndPoint("POPULAR");
+        this.fetchItems(endPoint, "POPULAR");
+        endPoint = this.getEndPoint("NOW_PLAYING");
+        this.fetchItems(endPoint, "NOW_PLAYING");
+        endPoint = this.getEndPoint("TOP_RATED");
+        this.fetchItems(endPoint, "TOP_RATED");
     }
 
     handleSearch = (searchTerm) => {
         
         let endPoint;
 
-        if(searchTerm === ''){
-            endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
-        } else {
-            endPoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
-        }
-        console.log(searchTerm);
         this.setState({
             movies: [],
-            loading: true,
+            nowPlayingMovies: [],
+            moviesLoading: true,
+            nowPlayingLoading: true,
             searchTerm
         });
-        this.fetchItems(endPoint);
+
+        if(searchTerm === ''){
+            endPoint = this.getEndPoint("POPULAR"); //`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
+            this.fetchItems(endPoint, "POPULAR");
+            endPoint = this.getEndPoint("NOW_PLAYING");
+            this.fetchItems(endPoint, "NOW_PLAYING");
+            endPoint = this.getEndPoint("TOP_RATED");
+            this.fetchItems(endPoint, "TOP_RATED");
+        } else {
+            endPoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+            this.fetchItems(endPoint, "SEARCH");
+        }
+        console.log(searchTerm);
+        
+        //this.fetchItems(endPoint);
     }
 
-    fetchItems = (endPoint) => {
+    fetchItems = (endPoint, term) => {
         fetch(endPoint)
             .then(res => res.json())
             .then(res => {
                 console.log(res);
-                this.setState({
-                    movies: [...this.state.movies, ...res.results], // concatenate the existing movies with the results
-                    welcomeImage: this.state.welcomeImage || {
-                        backdrop_path: res.results[1].backdrop_path,
-                        overview: res.results[1].overview,
-                        title: res.results[1].title
-                    },
-                    loading: false, // used to remove spinner gif and instead show the results
-                    currentPage: res.page, // get the current page of the result
-                    totalPages: res.total_pages // get total pages for results
-                });
+                if(term === "POPULAR") {
+                    this.setState({
+                        movies: [...this.state.movies, ...res.results], // concatenate the existing movies with the results
+                        welcomeImage: this.state.welcomeImage || {
+                            backdrop_path: res.results[1].backdrop_path,
+                            overview: res.results[1].overview,
+                            title: res.results[1].title
+                        },
+                        moviesLoading: false, // used to remove spinner gif and instead show the results
+                        moviesCurrentPage: res.page, // get the current page of the result
+                        moviesTotalPages: res.total_pages // get total pages for results
+                    });
+                } else if(term === "NOW_PLAYING") {
+                    this.setState({
+                        nowPlayingMovies: [...this.state.nowPlayingMovies, ...res.results], // concatenate the existing movies with the results
+                        nowPlayingLoading: false, // used to remove spinner gif and instead show the results
+                        nowPlayingCurrentPage: res.page, // get the current page of the result
+                        nowPlayingTotalPages: res.total_pages // get total pages for results
+                    });
+                } else if(term === "SEARCH") {
+                    this.setState({
+                        movies: [...res.results],
+                        nowPlayingMovies: [], // concatenate the existing movies with the results
+                        moviesLoading: false, // used to remove spinner gif and instead show the results
+                        nowPlayingLoading: false,
+                        nowPlayingCurrentPage: res.page, // get the current page of the result
+                        nowPlayingTotalPages: res.total_pages // get total pages for results
+                    });
+                } else if(term === "TOP_RATED") {
+                    this.setState({
+                        topRatedMovies: [this.state.topRatedMovies, ...res.results],
+                        topRatedLoading: false,
+                        topRatedCurrentPage: res.page, // get the current page of the result
+                        topRatedTotalPages: res.total_pages // get total pages for results
+                    });
+                }
+                
             });
     };
 
-    loadMore = () => {
+    loadMore = (term) => {
         let endPoint = '';
         this.setState({
-            loading: true
+            moviesLoading: true,
+            nowPlayingLoading: true
         });
+        
         if(this.state.searchTerm === ''){
-            endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+            switch(term) {
+                case "POPULAR":
+                        endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.moviesCurrentPage + 1}`;
+                        this.fetchItems(endPoint, "POPULAR");   
+                        break;
+                case "NOW_PLAYING":
+                        endPoint = `${API_URL}movie/now_playing?api_key=${API_KEY}&language=en-US&page=${this.state.nowPlayingCurrentPage + 1}&region=IN`;
+                        this.fetchItems(endPoint, "NOW_PLAYING");  
+                        break;
+                case "TOP_RATED":
+                        endPoint = `${API_URL}movie/top_rated?api_key=${API_KEY}&language=en-US&page=${this.state.topRatedCurrentPage + 1}`;
+                        this.fetchItems(endPoint, "TOP_RATED");  
+                        break;
+                default:
+                    break;
+            }
+            
         } else {
-            endPoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+            endPoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.moviesCurrentPage + 1}`;
+            this.fetchItems(endPoint, "SEARCH");
         }
-        this.fetchItems(endPoint);               
+                       
     }
 
     getEndPoint = (term) => {
         switch(term){
             case "POPULAR":
                 return `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+            case "NOW_PLAYING":
+                return `${API_URL}movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=IN`;
+            case "TOP_RATED":
+                return `${API_URL}movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
             case "WELCOME_IMAGE":
                 return `${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.welcomeImage.backdrop_path}`;
             default:
@@ -96,25 +168,68 @@ class Home extends Component {
         }
     }
 
-    getMovies = () => {
-        let movies = this.state.movies.map((ele, i) => {
-            return <Thumbnail 
-                        key={ele.id}
-                        clickable={true}
-                        image={ele.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${ele.poster_path}` : './images/nothumbnail.jpg'}
-                        itemId={ele.id}
-                        itemName={ele.original_title}
-
-                        />
-        });
-        if(this.state.loading){
-            movies.push(<Spinner />);
+    getMovies = (term) => {
+        let movies = [];
+        if(term === "POPULAR") {
+            movies = this.state.movies.map((ele, i) => {
+                return <Thumbnail 
+                            key={ele.id}
+                            clickable={true}
+                            image={ele.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${ele.poster_path}` : './images/nothumbnail.jpg'}
+                            itemId={ele.id}
+                            itemName={ele.original_title}
+    
+                            />
+            });
+            if(this.state.moviesLoading){
+                movies.push(<Spinner key="spinner_popular" />);
+            }
+            else if(this.state.movies.length && (this.state.moviesCurrentPage < this.state.moviesTotalPages) && !this.state.moviesLoading) {
+                movies.push(
+                    <LoadMore key="loading_popular" image={'./images/loadmore.jpg'} handleLoadMore={() => this.loadMore("POPULAR")}/>
+                );
+            }
+        } else if(term === "NOW_PLAYING") {
+            movies = this.state.nowPlayingMovies.map((ele, i) => {
+                return <Thumbnail 
+                            key={ele.id}
+                            clickable={true}
+                            image={ele.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${ele.poster_path}` : './images/nothumbnail.jpg'}
+                            itemId={ele.id}
+                            itemName={ele.original_title}
+    
+                            />
+            });
+            if(this.state.nowPlayingLoading){
+                movies.push(<Spinner key="spinner_nowplaying"/>);
+            }
+            else if(this.state.nowPlayingMovies.length && (this.state.nowPlayingCurrentPage < this.state.nowPlayingTotalPages) && !this.state.nowPlayingLoading) {
+                movies.push(
+                    <LoadMore key="loading_nowplaying"  image={'./images/loadmore.jpg'} handleLoadMore={() => this.loadMore("NOW_PLAYING")}/>
+                );
+            }
+        } else if(term === "TOP_RATED") {
+            movies = this.state.topRatedMovies.map((ele, i) => {
+                return <Thumbnail 
+                            key={ele.id}
+                            clickable={true}
+                            image={ele.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${ele.poster_path}` : './images/nothumbnail.jpg'}
+                            itemId={ele.id}
+                            itemName={ele.original_title}
+    
+                            />
+            });
+            if(this.state.topRatedLoading){
+                movies.push(<Spinner key="spinner_nowplaying"/>);
+            }
+            else if(this.state.topRatedMovies.length && (this.state.topRatedCurrentPage < this.state.topRatedTotalPages) && !this.state.topRatedLoading) {
+                movies.push(
+                    <LoadMore key="loading_nowplaying"  image={'./images/loadmore.jpg'} handleLoadMore={() => this.loadMore("NOW_PLAYING")}/>
+                );
+            }
         }
-        else if(this.state.movies.length && (this.state.currentPage < this.state.totalPages) && !this.state.loading) {
-            movies.push(
-                <LoadMore  image={'./images/loadmore.jpg'} handleLoadMore={this.loadMore}/>
-            );
-        }
+        
+        
         
         return movies;
     }
@@ -138,18 +253,41 @@ class Home extends Component {
                 <div className="home-grid-container">
                     <Grid 
                         header={this.state.searchTerm ? 'Search Results' : 'Popular Movies'}
-                        loading={this.state.loading}                        
+                        loading={this.state.moviesLoading}                        
                     >
                         {
-                            this.getMovies()
+                            this.getMovies("POPULAR")
                         }
                     </Grid>
                 </div>
+                {!this.state.searchTerm ? 
+                    <React.Fragment>
+                        <div className="home-grid-container">
+                            <Grid 
+                                header={'Now Playing'}
+                                loading={this.state.nowPlayingLoading}                        
+                            >
+                                {
+                                    this.getMovies("NOW_PLAYING")
+                                }
+                            </Grid>
+                        </div>
+                        <div className="home-grid-container">
+                            <Grid 
+                                header={'Top Rated'}                        
+                            >
+                                {
+                                    this.getMovies("TOP_RATED")
+                                }
+                            </Grid>
+                        </div>
+                    </React.Fragment>
+                : null}
                 {/* {this.state.loading ? <Spinner /> : null}
-                {(this.state.movies.length && this.state.currentPage < this.state.totalPages && !this.state.loading) ? 
+                {(this.state.movies.length && this.state.moviesCurrentPage < this.state.moviesTotalPages && !this.state.loading) ? 
                     <LoadMore  image={'./images/loadmore.jpg'} handleLoadMore={this.loadMore}/> : null
                 } */}
-                {!this.state.loading && !this.state.movies.length ? <NoResults image="https://image.flaticon.com/icons/svg/138/138366.svg" text="Nothing here fellas. Move on!"/> : null}
+                {!this.state.moviesLoading && !this.state.movies.length ? <NoResults image="https://image.flaticon.com/icons/svg/138/138366.svg" text="Nothing here fellas. Move on!"/> : null}
                                 
             </div>
         );
