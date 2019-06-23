96929bdd9384fc3643fa7b3c579915a5195ce0c9
Muhammed Nuhman <nuhman7@outlook.com>
2019-06-23 12:28:22 +0530

    add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Movie from './components/Movie/Movie';
 import Profile from './components/Profile/Profile';
@@ -18,6 +19,7 @@ function App() {
           <Route path="/profile/:itemId" component={Profile} exact />
           <Route component={NotFound} />
         </Switch>
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React ,{Component} from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    state = {
+
+    }
+    render(){
+        return(
+            <div className="footer-container">
+                    <footer>
+                        <p>Powered by <a target="_blank" rel="noopener noreferrer" href="https://www.themoviedb.org/en">TMDB</a></p>
+                        <p>Fork this on <a target="_blank" rel="noopener noreferrer" href="https://github.com/nuhman/alma">Github</a> <img src="https://image.flaticon.com/icons/svg/148/148836.svg" alt="love_logo" /></p>
+                    </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
